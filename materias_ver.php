dffe553df617747007ad9525597b8a0e2f949e9d
jaime12 <daniel931025@hotmail.com>
2015-04-21 12:35:13 -0700

    Agregue  pie de pagina....

--- materias_ver.php
+++ materias_ver.php
@@ -6,3 +6,6 @@
 <p> aqui se va  a ver  el nombre de la materia asi como tambien la descrpcion,video y direccion  de la pagina web de la materia</p>
 </body>
 </html>
+<?php
+include("pie.php");
+  ?>

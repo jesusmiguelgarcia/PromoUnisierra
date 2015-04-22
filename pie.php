27066db26e7e11e12b3f844a8721562f2eed116c
jaime12 <daniel931025@hotmail.com>
2015-04-22 12:56:36 -0700

    coloque  direccion .php

--- pie.php
+++ pie.php
@@ -5,10 +5,10 @@
       <p class="lf">© Copyright 2015 <a href="#">PromocionesUnisiera2015</a>. Layout by Rocket <a href="http://www.rocketwebsitetemplates.com/">Website Templates</a></p>
       <ul class="fmenu">
         <li class="active"><a href="index.html">Inicio</a></li>
-        <li><a href="materias.html">Materias</a></li>
-        <li><a href="actividades.html">Activiades</a></li>
-        <li><a href="login.html">Login</a></li>
-        <li><a href="ayuda.html">Ayuda</a></li>
+        <li><a href="materias.php">Materias</a></li>
+        <li><a href="actividades.php">Activiades</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="ayuda.php">Ayuda</a></li>
       </ul>
       <div class="clr"></div>
     </div>

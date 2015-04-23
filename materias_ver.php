8af07f2bf548f6583caa5d37d197150783e4b2ac
jaime <daniel931025@hotmail.com>
2015-04-23 12:58:33 -0600

    puse  la informacion de asignatura..

--- materias_ver.php
+++ materias_ver.php
@@ -1,11 +1,21 @@
-<html>
-<head>
-	<title> ver asignatura jaime </title>
-</head>
-<body>
-<p> aqui se va  a ver  el nombre de la materia asi como tambien la descrpcion,video y direccion  de la pagina web de la materia</p>
-</body>
-</html>
-<?php
-include("pie.php");
-  ?>
+
+<?php 
+include("conexion.php");
+ ?>
+ 
+	<?php
+
+	$conexion=mysql_connect($servidor,$usuario, $password);
+	mysql_select_db($bd);//bd
+	$sql="select nombre,descripcion,video,sitioWeb from materia";
+	$consulta=mysql_query($sql,$conexion);
+	 $resultado = mysql_fetch_array($consulta);
+	
+		echo "<b>Nombre: </b>".$resultado['nombre']."<b><br><br> Descripcion:<br> </b>:"
+		.$resultado["descripcion"].
+		":<br><br><b>Video: </b>".$resultado['video']. "<b><br><br>Sitio Web:</b><br><br><br>".$resultado['sitioWeb']."<br><br>";
+
+		
+	
+ ?>
+

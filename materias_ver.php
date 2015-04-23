cc4f3774270115686b8d330de39ca3422bb0a74c
jaime <daniel931025@hotmail.com>
2015-04-23 15:36:24 -0600

    ver  asignatura

--- materias_ver.php
+++ materias_ver.php
@@ -7,13 +7,13 @@ include("conexion.php");
 
 	$conexion=mysql_connect($servidor,$usuario, $password);
 	mysql_select_db($bd);//bd
-	$sql="select nombre,descripcion,video,sitioWeb from materia";
+	$sql="select nombre,descripcion,video,sitioweb from materia";
 	$consulta=mysql_query($sql,$conexion);
 	 $resultado = mysql_fetch_array($consulta);
 	
 		echo "<b>Nombre: </b>".$resultado['nombre']."<b><br><br> Descripcion:<br> </b>:"
 		.$resultado["descripcion"].
-		":<br><br><b>Video: </b><br>".$resultado['video']. "<b><br><br>Sitio Web:</b><br><br><br>".$resultado['sitioWeb']."<br><br>";
+		":<br><br><b>Video: </b><br>".$resultado['video']. "<b><br><br>Sitio Web:</b><br><br><br>".$resultado['sitioweb']."<br><br>";
 
 		
 	

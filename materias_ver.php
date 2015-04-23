1d625902c6eee5d6cc29acf51a4da095c9b0dd6c
jaime <daniel931025@hotmail.com>
2015-04-23 16:01:12 -0600

    arregle  lo del  video...

--- materias_ver.php
+++ materias_ver.php
@@ -1,21 +1,78 @@
+<html>
+<head>
+<title>Ayuda</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 <?php 
+include("cssyjs.php");
 include("conexion.php");
+$idmateria=2;
  ?>
- 
-	<?php
+</head>
 
-	$conexion=mysql_connect($servidor,$usuario, $password);
+<body>
+  <div class="todo">
+
+
+
+
+<?php 
+include("cabeza.php");
+
+$conexion=mysql_connect($servidor,$usuario, $password);
 	mysql_select_db($bd);//bd
 	$sql="select nombre,descripcion,video,sitioweb from materia";
 	$consulta=mysql_query($sql,$conexion);
 	 $resultado = mysql_fetch_array($consulta);
 	
-		echo "<b>Nombre: </b>".$resultado['nombre']."<b><br><br> Descripcion:<br> </b>:"
+ ?>
+
+
+
+ <div class="content">
+    <div class="content_resize">
+          <div class="article">
+
+          	diseño web
+          
+          	<?php
+
+	
+		echo "<h2>Nombre: ".$resultado['nombre']."</h2><p> Descripcion:<br>:"
 		.$resultado["descripcion"].
-		":<br><br><b>Video: </b><br>".$resultado['video']. "<b><br><br>Sitio Web:</b><br><br><br>".$resultado['sitioweb']."<br><br>";
+		":<br><br><b>Video: </b><br>".$resultado['video']. "<b><br><br>Sitio Web:</b><br><br>".$resultado['sitioweb']."<br><br>";
+
 
 		
 	
  ?>
 
+
+
+
+
+    </div>
+
+
+
+
+        
+  </div>
+      
+ 
+</div>
+
+  
+ <?php 
+include("pie.php");
+ ?>
+
+ 
+
+
+
+
+
+
+ 
+	

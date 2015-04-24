bed08f9a1ab0d0f3d7dcc3fbccc1776214309170
Yorlenis22 <yorlenisvaldez@gmail.com>
2015-04-24 10:17:17 -0600

    editando login

--- login_validar.php
+++ login_validar.php
@@ -1,8 +1,9 @@
 <?php
 	$nom=$_REQUEST['nombre'];
 	$contra=$_REQUEST['contrasena'];
-	
-	$conexion= mysql_connect("localhost","root") or die("Problemas al conectar con el servidor");
+	require('conexion.php');
+	$conexion = mysql_connect($servidor,$usuario,$password);
+	or die("Problemas al conectar con el servidor");
 	mysql_select_db("iscpromocion",$conexion) or die("Error al tratar de conectar con la base de datos");
 	$login=mysql_query("SELECT * FROM usuario where nombre = '".$nom."'and contrasena='".$contra."'",$conexion);
 
